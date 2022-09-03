09213bc4f4a975913992638e0531e376c9261e34
DasTab <dtabaldiev@gmail.com>
2022-09-04 01:58:54 +0600

    Reversed Words

--- script.js
+++ script.js
@@ -54,3 +54,21 @@ function generateShape(integer) {
 }
 
 // console.log(generateShape(8));
+
+// ========================================================================== //
+
+/* 
+Reversed Words
+Complete the solution so that it reverses all of the words within 
+the string passed in.
+Example(Input --> Output):
+"The greatest victory is that which requires no battle" --> 
+"battle no requires which that is victory greatest The"
+*/
+
+function reverseWords(str){
+
+  return str.split(' ').reverse().join(' ');
+}
+
+// console.log(reverseWords('Hello world!'));
