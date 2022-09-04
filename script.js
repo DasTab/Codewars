8f88e84652318c2641dc70dc88e8074f9162bddd
DasTab <dtabaldiev@gmail.com>
2022-09-05 02:14:15 +0600

    Debug celsius converter

--- script.js
+++ script.js
@@ -72,3 +72,34 @@ function reverseWords(str){
 }
 
 // console.log(reverseWords('Hello world!'));
+
+// ========================================================================== //
+
+/*
+Debug celsius converter
+Your friend is traveling abroad to the United States so he wrote a program 
+to convert fahrenheit to celsius. Unfortunately his code has some bugs.
+
+Find the errors in the code to get the celsius converter working properly.
+
+To convert fahrenheit to celsius:
+
+celsius = (fahrenheit - 32) * (5/9)
+*/
+
+function weatherInfo (temp) {
+  var c = convertToCelsius(temp);
+  return (c > 0) ? (c + " is above freezing temperature") : (c + " is freezing temperature");
+
+}
+
+function convertToCelsius (temperature) {
+  var celsius = (temperature - 32) * (5/9);
+  return celsius;
+}
+
+// console.log(convertToCelsius(50));
+// console.log(weatherInfo((50)));
+
+// ========================================================================== //
+
