79e1bc3d71c791e1e201c09029d0e146089abb01
DasTab <dtabaldiev@gmail.com>
2022-09-05 02:15:55 +0600

    Function factorial

--- script.js
+++ script.js
@@ -103,3 +103,20 @@ function convertToCelsius (temperature) {
 
 // ========================================================================== //
 
+/*
+Factorial
+Your task is to write function factorial
+*/
+function factorial(n) {
+  if (n <= 0) {
+      return 1;
+  } else {
+      return n * factorial(n-1);
+  }
+}
+
+// console.log(factorial(7));
+
+// ========================================================================== //
+
+
