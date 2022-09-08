b9179097458f3244b317c8bf1337af8dd6ad46dc
DasTab <dtabaldiev@gmail.com>
2022-09-08 21:25:15 +0600

    Remove anchor from URL

--- script.js
+++ script.js
@@ -167,4 +167,21 @@ function breakChocolate(n,m) {
   : n * m - 1;
 }
 
-console.log(breakChocolate(-5, 1));
+// console.log(breakChocolate(5, 5));
+
+// ========================================================================== //
+
+/*
+Remove anchor from URL
+Complete the function/method so that it returns the url with anything after 
+the anchor (#) removed.
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+*/
+
+function removeUrlAnchor(url){
+  return url.split('#')[0];
+}
+
+// console.log(removeUrlAnchor("www.codewars.com/katas/?page=1#about"));
