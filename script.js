19944b12330bc5a57ee77a65c87e255f740243fd
DasTab <dtabaldiev@gmail.com>
2022-10-09 23:28:31 +0600

    V A P O R C O D E

--- script.js
+++ script.js
@@ -219,7 +219,24 @@ For str = "krishan", the output should be "nahsirk".
 For str = "ultr53o?n", the output should be "nortlu".
  */
 function reverseLetter(str) {
-  return str.replace(/[^A-Za-z]/g, "").split("").reverse().join("");
+  return str.replace(/[^A-Za-z]/g, "").split("").reverse();
 }
 
 // console.log(reverseLetter("ultr53o?n"));
+
+// ========================================================================== //
+
+/*
+V A P O R C O D E
+Write a function that converts any sentence into a V A P O R W A V E sentence. 
+a V A P O R W A V E sentence converts all the letters into uppercase, 
+and adds 2 spaces between each letter (or special character) to create 
+this V A P O R W A V E effect.
+Examples
+"Lets go to the movies" -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+ */
+function vaporcode(string) {
+  return string.replace(/[^A-Za-z]/g, "").toUpperCase().split('').join("  ");
+} 
+
+// console.log(vaporcode("Lets go to the movies"));
